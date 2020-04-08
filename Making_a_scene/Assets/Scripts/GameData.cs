9e1a29855eeac29504e45024360e7057abc0a7cb
PsiMaster <rulkens.renee@gmail.com>
2020-04-08 23:18:11 +0200

    .

---
+++ Making_a_scene/Assets/Scripts/GameData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GameData File", menuName = "GameData File")]
+public class GameData : ScriptableObject
+{
+    public string playerOneName;
+    public string playerTwoName;
+
+    public int rounds;
+}
---
+++ Making_a_scene/Assets/Scripts/UI/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameData GameDataFile;
+    
+    public TMP_InputField InputFieldPlayerOne;
+    public TMP_InputField InputFieldPlayerTwo;
+    
+    public void StartGame()
+    {
+        if(InputFieldPlayerOne.text != "" && InputFieldPlayerTwo.text != "")
+        {
+            GameDataFile.playerOneName = InputFieldPlayerOne.text;
+            GameDataFile.playerTwoName = InputFieldPlayerTwo.name;
+            SceneManager.LoadScene(1);
+        }
+       
+    }
+}
